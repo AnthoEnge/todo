fa545dea1f7893e1b706f00c2161bf16796e6351
Deiyox <98240432+AnthoEnge@users.noreply.github.com>
2022-05-22 22:39:39 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+
+
 const buttonTodo = document.querySelector('.button');
 const buttonCreateTodo = document.querySelector('.buttonText');
 const buttonText = document.getElementById("text");
 const buttonValidate = document.querySelector('#valide');
 const todoRepetory = document.querySelector('.todo');
+const buttonComplete = document.querySelector('#completed');
+const buttonActive = document.querySelector("#active");
+const buttonAll = document.querySelector("#all");
 
 
 function CreateMenu() {
     buttonTodo.addEventListener('click', () => {
-        buttonCreateTodo.style.display = 'block';
+        buttonCreateTodo.classList.toggle('block');
+        buttonCreateTodo.classList.add('animation-target');
     });
 }CreateMenu();
 
 function RemoveMenu() {
     const remove = document.querySelector('#remove');
     remove.addEventListener('click', () => {
-        buttonCreateTodo.style.display = 'none';
+        buttonCreateTodo.classList.toggle('block');
     });
 } RemoveMenu();
 
@@ -26,8 +32,8 @@ let active = [];
 function CreateTodo() {
     buttonValidate.addEventListener('click', () => {
         let textAdd = buttonText.value;
-            if (textAdd === '' | textAdd === ' ') {
-                alert('Please enter a new todo');
+            if (textAdd === '' | textAdd === ' ' | textAdd.length > 30) {
+                alert('Please enter a new todo and don\'t exceed 30 characters');
             } else {
                 todoRepetory.style.display = 'block';
                 // create a new div
@@ -37,27 +43,40 @@ function CreateTodo() {
                 textTodo.innerHTML = textAdd;
                 const circle = document.createElement('input');
                 circle.classList.add('circle');
+
+                
+                    const divHours = document.createElement('div');
+                    divHours.classList.add('divHours');
+                    const hours = document.createElement('p');
+                    hours.classList.add('hours');
+                    var currentTime = new Date();
+                    hours.innerHTML = currentTime.getHours() + " h " + currentTime.getMinutes() + " m ";
+                    item.appendChild(divHours);
+                    divHours.appendChild(hours);
+                
+
+                item.appendChild(textTodo);
                 // Remove Item
                 const removeItem = document.createElement('button');
                 removeItem.classList.add('removeClick');
                 removeItem.addEventListener('click', () => {
-                    active.splice(active.indexOf(textAdd), 1);
-                    completedItem.push(item); // ADD TO COMPLETED
-                    item.style.backgroundColor = '#3d2db4';
+                    item.classList.add("bgRemoved");
+                    divHours.innerHTML = "removed";
+                    divHours.style.padding = "5px";
+                    divHours.style.fontSize = "10px";
                     item.removeChild(removeItem);
                     item.remove();
                     numberItem--;
                     numberCompletedItem++;
                     itemLeft.innerHTML = numberItem;
+                    completedItem.push(item); // ADD TO COMPLETED
+                    active.splice(active.indexOf(item), 1);
                 });
-                // Add Item in DOM
-                item.appendChild(circle);
-                item.appendChild(textTodo);
                 item.appendChild(removeItem);
-
+                    
                 active.push(item); // add item to active array
-                for ( activ of active) {
-                    todoRepetory.appendChild(activ);
+                for ( items of active) {
+                    todoRepetory.appendChild(items);
                 }
 
                 numberItem++;
@@ -66,21 +85,34 @@ function CreateTodo() {
                 itemLeft.innerHTML = numberItem;
             }
     });
+} CreateTodo();
 
-    const completedButton = document.querySelector('#completed');
-    completedButton.addEventListener('click', () => {
+
+
+function FilterMenu() {
+    
+    buttonComplete.addEventListener('click', () => {
+        buttonComplete.classList.add('menuActive');
+        buttonAll.classList.remove('menuActive');
+        buttonActive.classList.remove('menuActive');
         for ( complete of completedItem) {
             todoRepetory.appendChild(complete);
         }
         for ( activ of active) {
             activ.remove();
-            // todoRepetory.removeChild(activ);
-        } 
+        }
+        // active = [];
         itemLeft.innerHTML = numberCompletedItem;
+        removeCompleted.style.opacity = "1";
     });
 
-    const buttonActive = document.querySelector("#active");
+
+    
     buttonActive.addEventListener('click', () => {
+        removeCompleted.style.opacity = "0";
+        buttonActive.classList.add('menuActive');
+        buttonAll.classList.remove('menuActive');
+        buttonComplete.classList.remove('menuActive');
         for ( activ of active) {
             todoRepetory.appendChild(activ);
         }
@@ -91,8 +123,12 @@ function CreateTodo() {
         itemLeft.innerHTML = numberItem;
     });
 
-    const buttonAll = document.querySelector("#all");
+    
     buttonAll.addEventListener('click', () => {
+        removeCompleted.style.opacity = "0";
+        buttonAll.classList.add('menuActive');
+        buttonActive.classList.remove('menuActive');
+        buttonComplete.classList.remove('menuActive');
         for ( activ of active) {
             todoRepetory.appendChild(activ);
         }
@@ -101,16 +137,26 @@ function CreateTodo() {
         }
         itemLeft.innerHTML = numberItem + numberCompletedItem;
     });
-
     const removeCompleted = document.querySelector("#removeAll");
+    removeCompleted.style.opacity = "0";
     removeCompleted.addEventListener('click', () => {
+        removeCompleted.classList.add('menuActive');
+        setTimeout(delay, 100)
         for ( complete of completedItem) {
-            complete.remove(complete);
+            complete.remove();
+            numberCompletedItem--;
         }
-
         completedItem = [];
-        numberCompletedItem = 0;
+        itemLeft.innerHTML = numberCompletedItem;
+
+        function delay() {
+            removeCompleted.classList.remove('menuActive');
+        }
     });
 
-}CreateTodo();
 
+}FilterMenu();
+
+
+
+    
